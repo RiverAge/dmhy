26b76f3f968c38f235ef89ceb67e9bcdf0c83657
李蒙@Mac Pro <riverage@163.com>
2020-03-22 12:03:10 +0800

    TorrentRepository 的 注入

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,6 @@
 
 import 'package:dmhy/repository/login/login_respository.dart';
+import 'package:dmhy/repository/torrent/torrent_repository.dart';
 import 'package:dmhy/repository/user/user_repository.dart';
 import 'package:get_it/get_it.dart';
 
@@ -7,4 +8,5 @@ GetIt sl = GetIt.instance;
 void setup() {
   sl.registerSingleton<UserRepository>(UserRepository());
   sl.registerSingleton<LoginRepository>(LoginRepository());
+  sl.registerSingleton<TorrentRepository>(TorrentRepository());
 }
